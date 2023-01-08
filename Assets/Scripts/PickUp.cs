cf6c890d4862c3be37036435e659af2a255c85f5
coshibaer <116062853+coshibaer@users.noreply.github.com>
2023-01-08 16:32:50 +0100

    Labyrinth weiter gebaut, Pick Up für Chickens erstellt, Chickens erst mal Random auf der Map verteilt

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public Transform holdSpot; //where spot when hold
+    public LayerMask pickUpMask;
+    public Vector3 Direction { get; set ;}
+    private GameObject itemHolding;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) // E to PickUp
+        {
+            if(itemHolding) //Check if Item is holding
+            {
+                itemHolding.transform.position = transform.position + Direction; //to locate where to drop the item
+                itemHolding.transform.parent = null; //remove item from player
+                if (itemHolding.GetComponent<Rigidbody2D>())
+                    itemHolding.GetComponent<Rigidbody2D>().simulated = true; //check for rigidbody if exist
+                itemHolding = null;
+            }
+
+            else
+            {
+                Collider2D pickUpItem = Physics2D.OverlapCircle(transform.position + Direction, .1f, pickUpMask);
+                if (pickUpItem)
+                {
+                itemHolding = pickUpItem.gameObject;
+                itemHolding.transform.position = holdSpot.position; //item goes to spot
+                itemHolding.transform.parent = transform; //item will follow player
+                if (itemHolding.GetComponent<Rigidbody2D>()) //check if rigidbody2d is on item
+                    itemHolding.GetComponent<Rigidbody2D>().simulated = false;
+                }
+            }
+
+        }
+    }
+}
